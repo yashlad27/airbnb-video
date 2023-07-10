b571138ba8fbb3957dacb7a445bd1cbb4cf7e7a1
Yash Lad <yashlad727@gmail.com>
2023-07-10 19:42:25 +0530

    useState() added

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -2,8 +2,17 @@
 
 import { AiOutlineMenu } from 'react-icons/ai';
 import Avatar from '../Avatar';
+import { useCallback, useState } from 'react';
+import MenuItem from './MenuItem';
 
 const UserMenu = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleOpen = useCallback(() => {
+        setIsOpen((value) => !value);
+    }, []);
+
     return (
         <div className="relative">
             <div className="flex flex-row items-center gap-3">
@@ -24,7 +33,7 @@ const UserMenu = () => {
                     Airbnb your Home
                 </div>
                 <div
-                    onClick={() => {}}
+                    onClick={toggleOpen}
                     className="
                         p-4
                         md:py-1
@@ -41,13 +50,34 @@ const UserMenu = () => {
                         transition"
                 >
                     <AiOutlineMenu />
-
                     <div className="hidden md:block">
                         <Avatar />
                     </div>
 
                 </div>
             </div>
+
+            {isOpen && (
+                <div
+                    className='
+                        absolute
+                        rounded-xl
+                        shadow-md
+                        w-[40vw]
+                        bg-white
+                        overflow-hidden
+                        right-0
+                        top-12
+                        text-sm'
+                >
+                    <div className='flex flex-col cursor-pointer'>
+                        <>
+                            <MenuItem />
+                        </>
+                    </div>
+                </div>
+            
+            )}
         </div>
     );
 };
