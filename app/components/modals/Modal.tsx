9dff837eb7ca587c94f5104fdc4c63be06e59dbd
Yash Lad <yashlad727@gmail.com>
2023-07-13 12:13:43 +0530

    creating submit button

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -14,7 +14,7 @@ interface ModalProps {
     actionLabel?: string;
     disabled?: boolean;
     secondaryAction?: () => void;
-    secondaryLabel?: string;
+    secondaryActionLabel?: string;
 }
 const Modal: React.FC<ModalProps> = ({
     isOpen,
@@ -26,7 +26,7 @@ const Modal: React.FC<ModalProps> = ({
     actionLabel,
     disabled,
     secondaryAction,
-    secondaryLabel,
+    secondaryActionLabel,
 }) => {
     const [showModal, setShowModal] = useState(isOpen);
 
@@ -166,7 +166,20 @@ const Modal: React.FC<ModalProps> = ({
                                     gap-4 
                                     w-full
                                 '>
-                                    <Button />
+                                    {secondaryAction && secondaryActionLabel && (
+                                        <Button 
+                                        outline
+                                        disabled={disabled}
+                                        label={secondaryActionLabel}
+                                        onClick={handleSecondaryAction}
+                                        />
+                                    )}
+                        
+                                    <Button 
+                                        disabled={disabled}
+                                        label={actionLabel}
+                                        onClick={handleSubmit}
+                                    />
                                 </div>
                             </div>
                         </div>
